06cfd59997e3f6ec84491d4f536add35eaf0b9f3
AlxMen <alejandrocs402@gmail.com>
2024-04-30 13:16:29 +0100

    Creacion de las vistas para inicio y registro de usuarios

---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,22 @@
+import Logo from "@/components/Logo";
+import { Outlet } from "react-router-dom"
+
+export default function AuthLayout() {
+  return (
+    <>
+      <div className="bg-gray-800 min-h-screen">
+        <div className="py-10 lg:py-20 mx-auto w-[450px]">
+          <div className="flex w-72 mx-auto mb-5">
+            <Logo />
+          </div>
+          <p className="text-6xl text-center text-slate-100 font-black">
+            TaskMission
+          </p>
+          <div className="mt-10">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
