2612035da445a1b9979cdf39ea2ffe574d9a1545
AlxMen <alejandrocs402@gmail.com>
2024-05-07 13:13:35 +0100

    finalizacion de la funcion de recuperar contraseña

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,15 +1,22 @@
-import api from "@/lib/axios"
-import { isAxiosError } from "axios"
-import { ConfirmToken, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types"
+import api from "@/lib/axios";
+import { isAxiosError } from "axios";
+import {
+  ConfirmToken,
+  ForgotPasswordForm,
+  NewPasswordForm,
+  RequestConfirmationCodeForm,
+  UserLoginForm,
+  UserRegistrationForm,
+} from "../types";
 
 export async function createAccount(formData: UserRegistrationForm) {
   try {
-    const url = '/auth/create-account'
-    const { data } = await api.post<string>(url, formData)
-    return data
+    const url = "/auth/create-account";
+    const { data } = await api.post<string>(url, formData);
+    return data;
   } catch (error) {
     if (isAxiosError(error) && error.message) {
-      throw new Error(error.response?.data.error) 
+      throw new Error(error.response?.data.error);
     }
   }
 }
@@ -26,7 +33,9 @@ export async function confirmAccount(formData: ConfirmToken) {
   }
 }
 
-export async function requestConfirmationCode(formData: RequestConfirmationCodeForm) {
+export async function requestConfirmationCode(
+  formData: RequestConfirmationCodeForm
+) {
   try {
     const url = "/auth/request-code";
     const { data } = await api.post<string>(url, formData);
@@ -38,11 +47,51 @@ export async function requestConfirmationCode(formData: RequestConfirmationCodeF
   }
 }
 
-export async function authentcateUser(
-  formData: UserLoginForm
-) {
+export async function authentcateUser(formData: UserLoginForm) {
   try {
     const url = "/auth/login";
+    const { data } = await api.post<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.message) {
+      throw new Error(error.response?.data.error);
+    }
+  }
+}
+
+export async function forgotPassword(formData: ForgotPasswordForm) {
+  try {
+    const url = "/auth/forgot-password";
+    const { data } = await api.post<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.message) {
+      throw new Error(error.response?.data.error);
+    }
+  }
+}
+
+export async function validateToken(formData: ConfirmToken) {
+  try {
+    const url = "/auth/validate-token";
+    const { data } = await api.post<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.message) {
+      throw new Error(error.response?.data.error);
+    }
+  }
+}
+
+export async function updatePasswordWithToken({
+  formData,
+  token,
+}: {
+  formData: NewPasswordForm;
+  token: ConfirmToken['token'];
+}) {
+  try {
+    const url = `/auth/update-password/${token}`;
     const { data } = await api.post<string>(url, formData);
     return data;
   } catch (error) {
--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -1,6 +1,31 @@
+import NewPasswordForm from "@/components/auth/NewPasswordForm";
+import NewPasswordToken from "@/components/auth/NewPasswordToken";
+import { ConfirmToken } from "@/types/index";
+import { useState } from "react";
 
 export default function NewPasswordView() {
+  const [token, setToken] = useState<ConfirmToken["token"]>("");
+  const [isValidToken, setIsValidToken] = useState(false);
+
   return (
-    <div>NewPasswordView</div>
-  )
+    <>
+      <h1 className=" text-5xl font-black text-white text-center">
+        Reestablecer Contraseña
+      </h1>
+      <p className=" text-2xl font-light text-white mt-5">
+        Ingresa el codigo que recibiste {""}{" "}
+        <span className=" text-fuchsia-500 font-bold">por email</span>
+      </p>
+
+      {!isValidToken ? (
+        <NewPasswordToken
+          token={token}
+          setToken={setToken}
+          setIsValidToken={setIsValidToken}
+        />
+      ) : (
+        <NewPasswordForm token={token} />
+      )}
+    </>
+  );
 }
