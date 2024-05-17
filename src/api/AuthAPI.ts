e229694a4542cab600a989771a90cd509f4dcf83
AlxMen <alejandrocs402@gmail.com>
2024-05-17 13:27:59 +0100

    finalizacion de la funcionalidad de verificar eliminacion de proyecto

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,7 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
 import {
+  CheckPasswordForm,
   ConfirmToken,
   ForgotPasswordForm,
   NewPasswordForm,
@@ -109,6 +110,18 @@ export async function getUser() {
     if (response.success) {
       return response.data
     }
+  } catch (error) {
+    if (isAxiosError(error) && error.message) {
+      throw new Error(error.response?.data.error);
+    }
+  }
+}
+
+export async function checkPassword(formData: CheckPasswordForm) {
+  try {
+    const url = '/auth/check-password'
+    const { data } = await api.post<string>(url, formData)
+    return data
   } catch (error) {
     if (isAxiosError(error) && error.message) {
       throw new Error(error.response?.data.error);
